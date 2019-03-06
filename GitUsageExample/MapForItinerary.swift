bb635567dd145b8562e538f892114d2849238716
Coskun DURUKAN <coskundurukan@gmail.com>
2019-03-06 15:05:43 +0300

    Map Feature will be ready...

---
+++ GitUsageExample/MapForItinerary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItinerary.swift
+//  GitUsageExample
+//
+//  Created by Coşkun DURUKAN on 6.03.2019.
+//  Copyright © 2019 IC-TECH. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject
+{
+    
+}
